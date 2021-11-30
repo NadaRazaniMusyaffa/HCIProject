ba6d53b1bf44f4fecd1521e06e5c77387bef5f1d
Ahmad Reyhan <90187701+AReyhan@users.noreply.github.com>
2021-11-30 21:37:57 +0700

    Delete root/Sprint 2 directory

--- root/Sprint 2/pakaian.php
+++
@@ -1,5 +0,0 @@
-<?php 
-session_start();
-$_SESSION["category"] = "Pakaian";
-header("Location: category.php");
- ?>
--- root/Sprint 2/sport.php
+++
@@ -1,5 +0,0 @@
-<?php 
-session_start();
-$_SESSION["category"] = "Sport";
-header("Location: category.php");
- ?>
